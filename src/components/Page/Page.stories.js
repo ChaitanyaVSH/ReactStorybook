e31344033888465594c9829c80890e0b4d148dbc
Chaitanya <kotichai@amazon.com>
2021-03-03 00:51:41 +0530

    Created sample Page with small small stories

---
+++ src/components/Page/Page.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Small } from "../Input/Input.stories";
+import { Primary } from "../Button/Button.stories";
+
+export default {
+    title: "Page",
+}
+
+export const MainPage = () => (
+    <>
+        <Small/>
+        <br/>
+        <Small/>
+        <br/>
+        <Primary/>
+    </>
+)
